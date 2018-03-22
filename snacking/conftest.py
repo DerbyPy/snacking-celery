d55dc949fb4495a3e2ee8aec748f44f5e6bcd931
Randy Syring <randy@thesyrings.us>
2018-03-22 17:52:44 -0400

    fix tests

--- snacking/conftest.py
+++ snacking/conftest.py
@@ -1,11 +1,16 @@
 import pytest
 
-from snacking.celery.app import create_config
+from snacking.celery.app import app, apply_config, create_config
 import snacking.celery.testing as celery_testing
+
+
+def pytest_configure(config):
+    apply_config(app, '__tests__')
 
 
 @pytest.fixture(scope='session')
 def celery_config():
     """ Need to setup custom task annotations so the task tracker works correctly. """
     annotations = {'*': {'after_return': celery_testing.after_return_handler}}
-    return create_config('__tests__', task_annotations=annotations)
+    config = create_config('__tests__', task_annotations=annotations)
+    return config
